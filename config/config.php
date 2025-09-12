1cf1a4a942a64467c30fd072a657b7e56ee4942d
Sebastien Houssein <housseinseb@icloud.com>
2025-09-12 11:40:04 +0200

    Removing configs datas

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@ date_default_timezone_set('Europe/Paris');
 define('TEMPLATE_VIEW_PATH', ROOT_URL . '/app/views/templates/');
 define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php');
 
-define('DB_HOST', '127.0.0.1:8889');
-define('DB_NAME', 'opc_tomtroc');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
